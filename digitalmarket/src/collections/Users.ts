de0be975496fbb4b74888a28bde75cc1d17c2f37
Shreejan Joshi <shreejanjoshi97@gmail.com>
2024-04-05 15:25:00 +0200

    working on verification email

--- digitalmarket/src/collections/Users.ts
+++ digitalmarket/src/collections/Users.ts
@@ -3,7 +3,13 @@ import { CollectionConfig } from "payload/types";
 export const Users: CollectionConfig = {
   // same as collection name in lower case
   slug: "users",
-  auth: true,
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `<a href="${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}">Verify account</a>`;
+      },
+    },
+  },
   // only right people can see right data
   access: {
     // true any one can create or read
--- digitalmarket/src/get-payload.ts
+++ digitalmarket/src/get-payload.ts
@@ -46,6 +46,12 @@ export const getPayloadClient = async ({
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        // domain email
+        fromAddress: "onboarding@resend.dev",
+        fromName: "DigitalMarket"
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
